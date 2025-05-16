2f39e7bc51247dd0fac50041f79ab0f59e3098d7
root <root@LAPTOP-AHGV083G.>
2025-05-17 00:10:07 +0700

    add dann file

---
+++ dann.py
@@ -0,0 +1,18 @@
+# namabenar ="ananta barus"
+# datanim ="454545"
+
+# nama= input("masukkan nama mu jing :")
+# nim =input("masukkan nim mu toll :")
+
+# if nama== namabenar and nim== datanim:
+#     print("adalah mahasiwa kontol")
+# else:
+#     print("bukan mahasiwa kau kontol")
+
+# namasiswa= input("masukkan nama mu cok :")
+# nilaisiswa= float(input("masukkan nilai :"))
+# if nilaisiswa >= 85:
+#     print(f"{namasiswa} dinyatakan lulus seleksi dengan {nilaisiswa}")
+# else:
+#     print(f"{namasiswa} dinyatakan tidak lulus seleksi dengan {nilaisiswa}")
+
